0b1379ff3e45b128a07072d782b2d7cde2f296a5
pythonpanda2 <46133994+pythonpanda2@users.noreply.github.com>
2021-06-09 21:55:41 -0500

    Update run_mixedT_ECG.py

--- workflow/run_mixedT_ECG.py
+++ workflow/run_mixedT_ECG.py
@@ -1,3 +1,6 @@
+"""
+@author: gsivaraman@anl.gov
+"""
 from ECGMOD.preprocess import prepare_data, return_tensor
 from ECGMOD.multiT_preprocess import multiT_data_loader
 from ECGMOD.OTFSample import OTFSampler
